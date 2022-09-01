26d52083feb4c1eb7969a74f9fe26da456229599
M6Irinel <momoloacairinel666@gmail.com>
2022-09-01 14:26:20 +0200

    first commit

---
+++ assets/vue.js
@@ -0,0 +1,21 @@
+new Vue( {
+    el: '#app',
+    data () {
+        return {
+            arrayMail: []
+        }
+    },
+    methods: {
+        go10Mail () {
+            this.arrayMail = [];
+            
+            for ( let i = 0; i < 10; i++ ) {
+                axios.get( 'https://flynn.boolean.careers/exercises/api/random/mail' )
+                .then( ( res ) => this.arrayMail.push( res.data.response ) );
+            }
+        }
+    },
+    beforeMount() {
+        this.go10Mail();
+    },
+} );
