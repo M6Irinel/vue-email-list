64bf10af0d335c5e86a4cb0e1fe42684cd15e86d
M6Irinel <momoloacairinel666@gmail.com>
2022-10-11 23:13:03 +0200

    add redo all

--- assets/vue.js
+++ assets/vue.js
@@ -1,21 +1,25 @@
+// @ts-nocheck
 new Vue( {
     el: '#app',
+
     data () {
         return {
             arrayMail: []
         }
     },
+
     methods: {
         go10Mail () {
             this.arrayMail = [];
-            
+
             for ( let i = 0; i < 10; i++ ) {
                 axios.get( 'https://flynn.boolean.careers/exercises/api/random/mail' )
-                .then( ( res ) => this.arrayMail.push( res.data.response ) );
+                    .then( ( res ) => this.arrayMail.push( res.data.response ) );
             }
         }
     },
-    beforeMount() {
+
+    beforeMount () {
         this.go10Mail();
     },
 } );
